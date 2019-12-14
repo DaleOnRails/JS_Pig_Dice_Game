e13180a9054d59890fcd1becde2bda3667f69a40
Dalep <dalepaulsen.code.social@gmail.com>
2019-12-14 21:37:37 +1000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,119 @@
+/*  ************* THE "DOM" EXPLAINED *********************
+***********************************************************
+***********************************************************
+
+>>>>>>>>>>>> SHORT 
+> HTML content is stored in the DOM. 
+
+> The DOM is an interface that allows JS to interact with the webpage (HTML code). 
+
+> JS interacts with the DOM using special methods (DOM methods).
+
+
+
+>>>>>>>>>>>> LONG
+-------------------------------------------
+WHAT IS DOM MANIPULATON? 
+-------------------------------------------
+> DOM manipulation simply refers to getting JS code to interact with the browser (HTML)
+
+-------------------------------------------
+WHAT IS THE DOM (Document Object Model)? 
+-------------------------------------------
+> Used to connect webpages to scripts such as JAVASCRIPT
+
+> Is a structured representation of a HTML document
+
+> Structure refers to the structure of a HTML Doc, like: 
+(<body> ... </body>, <p> ... </p> etc, etc, .....)
+
+> Think of each HTML tag/element as a box. 
+> & boxes within other boxes, example: (link tag inside of a paragraph tag)
+
+> So.. the DOM gives me a fully OBJECT ORIENTATED representation of a HTML doc.
+
+> For each of these boxes the DOM has an OBJECT for it.
+> I can then access these DOM objects from my JS code.
+
+> THIS IS HOW PARTS OF A HTML PAGE CAN GET FUNCTIONALITY AND MOVE AROUND LIKE ALL THE COOL EFFECTS I SEE. (particles.js)
+
+***IMPORTANT NOTE:***
+> Dont mix up the DOM and JS. They are two different things.
+
+> Think of DOM as a languange translator to allow JS & HTML to talk to eachother. 
+
+> He translates JS lang for HTML so that HTML can understand and RESPOND....
+
+> JS USES SPECIAL METHODS to change DOM objects. (and therefore the webpage itself). 
+
+> These methods are just like methods in normal JS (A function attached to an object literal), but instead.. the object they are attached to is the DOM.
+
+> SIMPLE
+*/
+
+
+/*  ************* BROWSER GAME - PIG *********************
+***********************************************************
+***********************************************************
+/*
+Goal of the game:
+- First to get a global score of 100 wins.
+
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he wishes. Each result gets added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+BASICALLY THE SAME AS BLACKJACK - but with dice:
+- if a '1' is rolled the score is a bust.
+- can 'hold' where you skip your turn. 
+*/
+
+
+
+/* Code comments to add:
+-------------------------------------------
+// How to create fundamental variables of the game
+// How to generate a random number
+// How to manipulate the DOM
+// DOM Manipulation 1
+// DOM Manipulation 2
+// DOM Manipulation 3
+// DOM Manipulation 4
+// DOM Manipulation 5
+// How to read from the DOM (Meaning, )
+// How to change CSS styles
+-------------------------------------------
+*/
+
+// Start of game code:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
